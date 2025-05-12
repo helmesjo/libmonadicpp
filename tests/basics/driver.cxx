338d5d23283865bb9d040e178a2c7a267be1c959
helmesjo <helmesjo@gmail.com>
2025-05-12 14:58:46 +0200

    TEMP: Working on an automagic `disambiguate()` method. Seems to be working nicely with clang/apple-clang, but gcc still complains on the `operator()` overload resolution.

--- tests/basics/driver.cxx
+++ tests/basics/driver.cxx
@@ -1,41 +1,91 @@
-#include <monadicpp/monad.hxx>
-#include <monadicpp/version.hxx>
+#include <type_traits>
 
-#include <string>
+#define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
 
-#undef NDEBUG
-#include <cassert>
+// Free function pointer
+template<typename... Args, typename... Inferred, typename R>
+constexpr auto
+disambiguate(R (*f)(Args..., Inferred...)) -> R (*)(Args..., Inferred...)
+{
+  return f;
+}
+
+// Non-const member function pointer
+template<typename... Args, typename... Inferred, typename R, typename C>
+constexpr auto
+disambiguate(R (C::*f)(Args..., Inferred...)) -> R (C::*)(Args..., Inferred...)
+{
+  return f;
+}
+
+// Const member function pointer
+template<typename... Args, typename... Inferred, typename R, typename C>
+constexpr auto
+disambiguate(R (C::*f)(Args..., Inferred...) const) -> R (C::*)(Args..., Inferred...) const
+{
+  return f;
+}
+
+// Functor/lambda: Resolve operator() implicitly with partial signature
+template<typename... Args, typename F>
+constexpr auto
+disambiguate(F&&) -> decltype(disambiguate<Args...>(&std::remove_reference_t<F>::operator()))
+{
+  return nullptr;
+}
+
+struct Dummy
+{
+  auto
+  operator()(int a, float b) -> int
+  {
+    return a + static_cast<int>(b);
+  }
+
+  auto
+  operator()(char a, float b) -> int
+  {
+    return static_cast<int>(a) + static_cast<int>(b);
+  }
+
+  auto
+  operator()(int a, double b, int c) -> int
+  {
+    return a + static_cast<int>(b) + static_cast<int>(c);
+  }
+};
+
+struct Class
+{
+  auto
+  method(int a, float b) -> int
+  {
+    return a + static_cast<int>(b);
+  }
+
+  [[nodiscard]] auto
+  const_method(int a, float b) const -> int
+  {
+    return a + static_cast<int>(b);
+  }
+};
+
+auto
+func(int a, float b) -> int
+{
+  return a + static_cast<int>(b);
+}
 
 auto
 main() -> int
 {
-  using namespace fho;
-
-  constexpr auto m1 = pure(
-    []()
-    {
-      return 0.0;
-    });
-  (void)m1;
-  constexpr auto m2 = pure(2);
-  constexpr auto m3 = m2.fmap(
-    [](int x)
-    {
-      return std::to_string(x);
-    });
-
-  constexpr auto m4 = m3.fmap(
-    [](std::string x) -> double
-    {
-      return std::stoi(x);
-    });
-
-  constexpr auto m5 = m4.bind(
-    [](double x)
-    {
-      return pure(x + 1);
-    });
-
-  assert(m5.value() + m4.value() == 5.0);
+  Dummy d;
+  auto  p1 = disambiguate<int, float>(d);  // int (Dummy::*)(int, float)
+  auto  p2 = disambiguate<char, float>(d); // int (Dummy::*)(char, float)
+  auto  p3 = disambiguate<int, double>(d); // int (Dummy::*)(int, double)
+  auto  p4 = disambiguate<int>(func);      // int (*)(int, float)
+  // Class c;
+  auto p5 = disambiguate<int>(&Class::method);       // int (Class::*)(int, float)
+  auto p6 = disambiguate<int>(&Class::const_method); // int (Class::*)(int, float) const
   return 0;
 }
